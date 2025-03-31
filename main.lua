a8bcebdbdaf9862e124c7631ee16c36c2b9bfb44
Ash <Arcytch@gmail.com>
2025-03-31 22:55:25 +0200

    First iteration of documentation. Main.lua is largely incomplete due to the code being easy to understand, or in need of changes.

--- main.lua
+++ main.lua
@@ -1,33 +1,24 @@
 
 
 
-local backgroundColor = {love.math.colorFromBytes(80,80,80)}
-local accentColor = {love.math.colorFromBytes(0,200,180)}
-
-
-
-require "buttonModule"
-
-
-
-
-
-
-
-
-
-
 
 
 
 
 function love.load()
 
+    require "buttonController"
+
     --love.window.setMode(1940,1080)
 
     screenHeight = love.graphics.getHeight()
     screenWidth = love.graphics.getWidth()
     
+    theme = {
+        backgroundColor = {love.math.colorFromBytes(80,80,80)},
+        accentColor = {love.math.colorFromBytes(0,200,180)},
+    }
+
     clickSfx = love.audio.newSource("Sfx/clickSFX1.mp3", "stream")
     
     fontBig = love.graphics.newFont("MyFont.ttf",50)
@@ -52,7 +43,7 @@ function love.load()
         y = uiPlacement.title.y + 100,
     }
 
-    button = buttonMaster.new("Start", uiPlacement.buttonStart.x, uiPlacement.buttonStart.y, 200,50, clickSfx)
+    button = buttonController.new("Start", uiPlacement.buttonStart.x, uiPlacement.buttonStart.y, 200,50, clickSfx)
 
 
 end
@@ -64,7 +55,7 @@ function drawText(text, x, y, limit, big)
     else
         love.graphics.setFont(fontSmall)
     end
-    love.graphics.setColor(accentColor)
+    love.graphics.setColor(theme.accentColor)
     love.graphics.printf(text,x,y,limit,"center")
     love.graphics.setFont(fontSmall)
 end
@@ -72,26 +63,26 @@ end
 
 function love.mousepressed(x, y, mouseButton, istouch, presses)
     if mouseButton == 1 then
-        for _, buttonObj in pairs(buttonMaster.buttons) do
-            buttonObj:click(x,y)
+        for _, buttonObj in pairs(buttonController.buttons) do
+            buttonObj:click(x,y) -- Change to make use of the hover property, instead of itterating through all buttons. :p 
+            -- (She means make use of the variable hover. In other words, if hover, that means we probably clicked that button.)
         end
     end
 end
 
 
 function love.update()
-    for _, buttonObj in pairs(buttonMaster.buttons) do -- Looping through all currently active buttons and updating their status.
-        buttonObj:update(love.mouse.getPosition())
-    end
+    buttonController.update(love.mouse.getPosition())
 end
 
 function love.draw()
-    love.graphics.setColor(backgroundColor)
+    love.graphics.setColor(theme.backgroundColor)
     love.graphics.rectangle("fill", 0, 0, screenWidth, screenHeight)
     love.graphics.setColor(255,255,255)
     drawText("Time is Money", uiPlacement.title.x, uiPlacement.title.y, 400, true)
     drawText("Version 0.0: Genesis", uiPlacement.underTitle.x, uiPlacement.underTitle.y, 250, false)
-    for _, buttonObj in pairs(buttonMaster.buttons) do -- Looping through all currently active buttons and updating their status.
+    
+    for _, buttonObj in pairs(buttonController.buttons) do -- Looping through all currently active buttons and updating their status.
         buttonObj:draw()
     end
 end
